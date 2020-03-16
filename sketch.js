b3f15785da5730bf5c1992321c13a78a55c41279
Pratyush <90097@Pratyushs-MacBook-Pro.local>
2020-03-16 15:26:26 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  createCanvas(600,400);
+}
+
+function draw() {
+  background(0);
+  stroke(155);
+  circle(300,200,100);
+  console.log("lalala");
+  // put drawing code here
+}
